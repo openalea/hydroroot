b69d50a38f666caa59541a298f7621f3f6d9c0ed
baugetfa <fabrice.bauget@cirad.fr>
2026-02-02 17:13:27 +0100

    revert previous commit

--- doc/conf.py
+++ doc/conf.py
@@ -41,8 +41,6 @@ extensions = [
     "nbsphinx",     # for integrating jupyter notebooks
     "myst_parser"   # for parsing .md files
 ]
-nbsphinx_execute = 'always'
-nbsphinx_timeout = 60
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 autosummary_generate = True
