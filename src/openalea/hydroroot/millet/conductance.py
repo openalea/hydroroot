1edc5e24b4c19cdb85df87ee7d900dbb45796f9d
NDOUR <adama.ndour@cimmyt.org>
2026-02-19 23:04:41 +0300

    updating architecture simulation and calculation of hydraulic architecture

--- src/openalea/hydroroot/millet/conductance.py
+++ src/openalea/hydroroot/millet/conductance.py
@@ -24,3 +24,32 @@ def fit_property_from_spline(g, spline, prop_in, prop_out):
 
     return g
 
+
+def compute_K_from_laws(g, seminal_axial_conductivity_law, crown_axial_conductivity_law, lateral_axial_conductivity_law):
+    """
+
+    compute the axial conductance K versus the vertex position according to some laws and to the root types: crown, seminal,
+    laterals
+
+    :param g: (MTG)
+
+    """
+    K={}
+
+
+    positions= g.property('relative_position')
+    orders = g.property('order')
+
+    for vid in g.vertices_iter(g.max_scale()):
+            if g.label(vid)=='Crown':
+                K[vid] = crown_axial_conductivity_law(positions[vid])
+            elif g.label(vid) == 'Seminal' :
+                K[vid] = seminal_axial_conductivity_law(positions[vid])
+            elif g.label(vid) == 'Lateral' :
+                    K[vid] = lateral_axial_conductivity_law(positions[vid])
+            else: # for the collet and other unknown tpes
+                K[vid] = seminal_axial_conductivity_law(positions[vid])
+
+    g.properties()['K'] = K
+
+    return g
