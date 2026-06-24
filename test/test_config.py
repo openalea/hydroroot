6c8d304b2addbe94be63f38f1c1533c05392c7a9
LeticiaNapolitano-34 <leticia.napolitano@etu.umontpellier.fr>
2026-06-24 15:13:50 +0200

    Test of the default config of HydroRoot

--- test/test_config.py
+++ test/test_config.py
@@ -1,10 +1,18 @@
-from openalea.hydroroot.init_parameter import Parameters
-from openalea.hydroroot.model_unit import HydroRootModelUnit
-from openalea.core.config import Config
+
+from openalea.hydroroot.init_parameter import Parameters, hydroroot_parameters_to_config
+from openalea.core.config import Config, ModelUnit, Parameter
+import yaml
+
 
 params = Parameters()
-unit = HydroRootModelUnit(params)
+cfg1 = hydroroot_parameters_to_config(params)
+cfg1.custom_comments["length_file"] = "length file"
+    
+cfg1.dump("hydro_config4.json")
+cfg1.dump("hydro.yml")
+cfg2 = Config([]).load("hydro_config4.json")
+assert dict(cfg1) == dict(cfg2)
 
-cfg = Config([unit])
 
-cfg.dump("hydro_config.json")
+
+
